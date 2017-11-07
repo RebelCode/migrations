7393b50f10bc1952d7499850f24dc87a16a17eae
Miguel Muscat <miguelmuscat93@gmail.com>
2017-11-07 12:48:34 +0100

    Added unit test for `MigratorInterface`

---
+++ test/unit/MigratorInterfaceTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace RebelCode\Migrations\UnitTest;
+
+use Xpmock\TestCase;
+use RebelCode\Migrations\MigratorInterface as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class MigratorInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\Migrations\MigratorInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                     ->up()
+                     ->down()
+                     ->reset();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
+}
