e960ccd655cd56bd507eb0c3b544e5855f1c66db
Miguel Muscat <miguelmuscat93@gmail.com>
2017-11-10 10:55:39 +0100

    Added `PdoSqliteDriverStub` for tests

---
+++ test/stub/PdoSqliteDriverStub.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RebelCode\Migrations\Stub;
+
+use ByJG\AnyDataset\DbDriverInterface;
+use ByJG\AnyDataset\Store\DbPdoDriver;
+use ByJG\Util\Uri;
+use PDO;
+
+/**
+ * An extension of ByJG's PDO driver that can be constructed without a URI.
+ *
+ * Used in conjunction with SQLite in-memory databases, that are accessed via a DBN that can't be parsed correctly by
+ * the URI class.
+ *
+ * @since [*next-version*]
+ */
+class PdoSqliteDriverStub extends DbPdoDriver implements DbDriverInterface
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param PDO    $pdo    The PDO instance.
+     * @param string $dbName Thee name of the database to connect to.
+     */
+    public function __construct(PDO $pdo, $dbName)
+    {
+        $this->instance      = $pdo;
+        $this->connectionUri = new Uri('mysql://root@memory/' . $dbName);
+    }
+}
